82a51d27bb77a51332596baf19b8c6d0bf1acd6f
root <webadmin@hostixe.com>
2012-10-12 17:06:09 +0300

    fix code formating

--- models/Tpl.php
+++ models/Tpl.php
@@ -1,6 +1,6 @@
 <?php
-	class Tpl extends Std
-	{
+    class Tpl extends Std
+    {
         private $globals = array('empty' => '');
 
         public function register($key, $value)
@@ -8,45 +8,45 @@
             $this->globals[$key] = $value;
         }
 
-		public function exists($tpl = '')
-		{
-			if(file_exists(DIR_VIEWS . $tpl . TEMPLATE_EXT))
-			{
-				return true;
+        public function exists($tpl = '')
+        {
+            if(file_exists(DIR_VIEWS . $tpl . TEMPLATE_EXT))
+            {
+                return true;
             }
             return false;
         }
 
-		public function render($tpl = '', $data = array(' ' => ' '))
-		{
-			if(!file_exists(DIR_VIEWS . $tpl . TEMPLATE_EXT))
-			{
-				return '{{error:template was not found:}}';
-			}
-			$tpl = file_get_contents(DIR_VIEWS . $tpl . TEMPLATE_EXT);
-			
+        public function render($tpl = '', $data = array(' ' => ' '))
+        {
+            if(!file_exists(DIR_VIEWS . $tpl . TEMPLATE_EXT))
+            {
+                return '{{error:template was not found:}}';
+            }
+            $tpl = file_get_contents(DIR_VIEWS . $tpl . TEMPLATE_EXT);
+
             //include other templates {{include:header}} with the same $data
-			if(preg_match_all('/.*?\{\{include\:(.*)\}\}.*?/i', $tpl, $params))
-			{
-				foreach($params[1] as $inc)
-				{
-					$tpl = str_replace('{{include:' . $inc . '}}',
-						$this->render($inc, $data), $tpl);
-				}
-			}
-            
+            if(preg_match_all('/.*?\{\{include\:(.*)\}\}.*?/i', $tpl, $params))
+            {
+                foreach($params[1] as $inc)
+                {
+                    $tpl = str_replace('{{include:' . $inc . '}}',
+                            $this->render($inc, $data), $tpl);
+                }
+            }
+
             //replace vars {{var}}
             $data = array_merge($this->globals, $data);
             foreach($data as $key => $val)
             {
                 $tpl = str_replace('{{' . $key . '}}', $val, $tpl);
             }
-			
+
             //{{if {{A}}={{B}} }}some{{else}}other{{fi}}
             if(preg_match_all('/^.+(\{\{if\s(.*?)(\=|\!\=)(.*?)\}\}(.+)\{\{else\}\}(.+)\{\{fi\}\}).+$/ims', $tpl, $params))
-			{
-				foreach($params[3] as $num => $cond)
-				{
+            {
+                foreach($params[3] as $num => $cond)
+                {
                     switch($cond)
                     {
                         case '=':
@@ -58,7 +58,7 @@
                             {
                                 $tpl = str_replace($params[1][$num], $params[6][$num], $tpl);
                             }
-                        break;
+                            break;
                         case '!=':
                             if($params[2][$num] != $params[4][$num])
                             {
@@ -68,21 +68,21 @@
                             {
                                 $tpl = str_replace($params[1][$num], $params[6][$num], $tpl);
                             }
-                        break;
+                            break;
                     }
-				}
-			}
+                }
+            }
 
             //hide all undefined tpl vars
             if(preg_match_all('/.*?\{\{(.*)\}\}.*?/i', $tpl, $op))
-			{
-				foreach($op[1] as $var)
-				{
-					$tpl = str_replace('{{' . $var . '}}', '', $tpl);
-				}
-			}
+            {
+                foreach($op[1] as $var)
+                {
+                    $tpl = str_replace('{{' . $var . '}}', '', $tpl);
+                }
+            }
 
             return $tpl;
-		}
-	}
+        }
+    }
 ?>
